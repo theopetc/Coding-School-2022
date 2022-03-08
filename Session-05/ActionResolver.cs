d275788861819e204bf53091ae5721229bc8c831
Theodoros Petsagkas <tpetsagkas@gmail.com>
2022-03-08 18:28:52 +0200

    Action resolver is now abstract class

--- Session-05/ActionResolver.cs
+++ Session-05/ActionResolver.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Session_05
 {
-    internal class ActionResolver
+    internal abstract class ActionResolver
     {
         public MessageLogger Logger = new MessageLogger();
         public ActionResolver()
@@ -14,8 +14,48 @@ namespace Session_05
 
         }
 
-        public ActionResponse Execute(ActionRequest request)
+        public abstract ActionResponse Execute(ActionRequest request);
+
+        public string CheckInput(string input)
         {
+            string result = "String";
+            int tryInt;
+            if (!string.IsNullOrEmpty(input))
+            {
+                if (Int32.TryParse(input, out tryInt))
+                {
+                    result = "Integer";
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your input is empty");                
+            }
+            return result;
+        }
+
+        internal void WriteMessage(Message msg)
+        {
+            try
+            {
+                Logger.Write(msg);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }                
+        }
+    }
+
+    internal class ConvertToBinary : ActionResolver
+    {
+        public ConvertToBinary()
+        {
+
+        }
+
+        public override ActionResponse Execute(ActionRequest request)
+        {//Why does it require override keyword if it's the only implementation
             var response = new ActionResponse();
             Message msg = new Message();
 
@@ -36,104 +76,86 @@ namespace Session_05
                 msg.MessageContent = response.Output;
                 WriteMessage(msg);
             }
-
-            if (request.Action == ActionEnum.Uppercase)
-            {
-                Console.Write("Enter a string: ");
-                string input = Console.ReadLine();
-
-                string[] words = input.Split(' ');
-
-                if ((CheckInput(input) != "Integer")  || words.Length >= 2) 
-                {
-                    msg.MessageContent = "Invalid input";
-                    //Logger.Write(msg);
-                    WriteMessage(msg);
-                }
-
-                string word = string.Empty;
-                int i = 0;
-                foreach (string s in words)
-                {
-                    if (s.Length > i)
-                    {
-                        word = s;
-                        i = s.Length;
-                    }
-                }
-                response.Output = "The longest word is " + word.ToUpper();
-                
-                msg.MessageContent = response.Output;
-                //Logger.Write(msg);
-                WriteMessage(msg);
-            }
-
-            if (request.Action == ActionEnum.Reverse)
-            {
-                Console.Write("Enter a string: ");
-                string input = Console.ReadLine();
-
-                if (CheckInput(input) != "Integer")
-                {
-                    msg.MessageContent = "Invalid input";
-                    WriteMessage(msg);
-                }
-                int stringLength = input.Length;
-                char[] result = new char[stringLength];
-
-                for (int i = 0; i < stringLength; i++)
-                {
-                    result[i] += input[stringLength - i - 1];
-                }
-
-                for (int i = 0; i < stringLength; i++)
-                {
-                    response.Output += Convert.ToString(result[i]);
-                }
-
-                msg.MessageContent = response.Output;               
-                WriteMessage(msg);
-            }
-            
             return response;
         }
 
-        public string CheckInput(string input)
-        {
-            string result = "String";
-            int tryInt;
-            if (!string.IsNullOrEmpty(input))
-            {
-                if (Int32.TryParse(input, out tryInt))
-                {
-                    result = "Integer";
-                }
-            }
-            else
-            {
-                Console.WriteLine("Your input is empty");                
-            }
-            return result;
-        }
-
-        private void Exit()
-        {
-            Console.WriteLine("Your input is invalid!");
-            Console.Write("Press any key to exit...");
-            Console.ReadLine();
-            Environment.Exit(0);
-        }
-
-        private void WriteMessage(Message msg)
-        {
-            try
-            {
-                Logger.Write(msg);
-            }
-            catch (NullReferenceException ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }                
-        }
     }
+
+    //internal class Uppercase : ActionResolver
+    //{
+    //    public ActionResponse Execute(ActionRequest request)
+    //    {
+    //        var response = new ActionResponse();
+    //        Message msg = new Message();
+
+    //        if (request.Action == ActionEnum.Uppercase)
+    //        {
+    //            Console.Write("Enter a string: ");
+    //            string input = Console.ReadLine();
+
+    //            string[] words = input.Split(' ');
+
+    //            if ((CheckInput(input) != "Integer")  || words.Length >= 2) 
+    //            {
+    //                msg.MessageContent = "Invalid input";
+    //                //Logger.Write(msg);
+    //                WriteMessage(msg);
+    //            }
+
+    //            string word = string.Empty;
+    //            int i = 0;
+    //            foreach (string s in words)
+    //            {
+    //                if (s.Length > i)
+    //                {
+    //                    word = s;
+    //                    i = s.Length;
+    //                }
+    //            }
+    //            response.Output = "The longest word is " + word.ToUpper();
+                
+    //            msg.MessageContent = response.Output;
+    //            //Logger.Write(msg);
+    //            WriteMessage(msg);
+    //        }
+    //    }
+    //}
+
+    //internal class Reverse : ActionResolver
+    //{
+    //    public ActionResponse Execute(ActionRequest request)
+    //    {
+    //        var response = new ActionResponse();
+    //        Message msg = new Message();
+
+    //        if (request.Action == ActionEnum.Reverse)
+    //        {
+    //            Console.Write("Enter a string: ");
+    //            string input = Console.ReadLine();
+
+    //            if (CheckInput(input) != "Integer")
+    //            {
+    //                msg.MessageContent = "Invalid input";
+    //                WriteMessage(msg);
+    //            }
+    //            int stringLength = input.Length;
+    //            char[] result = new char[stringLength];
+
+    //            for (int i = 0; i < stringLength; i++)
+    //            {
+    //                result[i] += input[stringLength - i - 1];
+    //            }
+
+    //            for (int i = 0; i < stringLength; i++)
+    //            {
+    //                response.Output += Convert.ToString(result[i]);
+    //            }
+
+    //            msg.MessageContent = response.Output;               
+    //            WriteMessage(msg);
+    //        }
+    //    }
+    //}
+
+        
 }
--- Session-05/Program.cs
+++ Session-05/Program.cs
@@ -10,33 +10,35 @@ namespace Session_05
     {
         static void Main(string[] args)
         {
-            ActionResolver a = new ActionResolver();
-            ActionRequest request = new ActionRequest();
-            ActionResponse response = new ActionResponse();
+            //ActionResolver a = new ActionResolver();
+            //ActionRequest request = new ActionRequest();
+            //ActionResponse response = new ActionResponse();
 
-            Console.Write("Choose Convert, Uppercase or Reverse: ");
-            string input = Console.ReadLine();
+            //Console.Write("Choose Convert, Uppercase or Reverse: ");
+            //string input = Console.ReadLine();
 
-            switch (input)
-            {
-                case "Convert":
-                    request.Action = ActionEnum.Convert;
-                    response = a.Execute(request);
-                    Console.WriteLine(response.Output);
-                    break;
-                case "Uppercase":
-                    request.Action = ActionEnum.Uppercase;
-                    response = a.Execute(request);
-                    Console.WriteLine(response.Output);
-                    break;
-                case "Reverse":
-                    request.Action = ActionEnum.Reverse;
-                    response = a.Execute(request);
-                    Console.WriteLine(response.Output);
-                    break;
-                default:
-                    throw new Exception();
-            }
+            //switch (input)
+            //{
+            //    case "Convert":
+            //        request.Action = ActionEnum.Convert;
+            //        response = a.Execute(request);
+            //        Console.WriteLine(response.Output);
+            //        break;
+            //    case "Uppercase":
+            //        request.Action = ActionEnum.Uppercase;
+            //        response = a.Execute(request);
+            //        Console.WriteLine(response.Output);
+            //        break;
+            //    case "Reverse":
+            //        request.Action = ActionEnum.Reverse;
+            //        response = a.Execute(request);
+            //        Console.WriteLine(response.Output);
+            //        break;
+            //    default:
+            //        throw new Exception();
+            //}
+
+
 
             Console.ReadLine();
         }
