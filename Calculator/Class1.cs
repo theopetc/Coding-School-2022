929a8dc3a61638dfc3d41dc630bec55daae74620
Theodoros Petsagkas <tpetsagkas@gmail.com>
2022-03-09 14:59:18 +0200

    Session-06

---
+++ Calculator/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public abstract class CalculatorBase
+    {
+        public CalculatorBase()
+        {
+
+        }
+        public abstract string Operation(decimal x, decimal y);
+    }
+    public class Addition : CalculatorBase
+    {
+        public Addition()
+        {
+
+        }
+
+        public override string Operation(decimal x, decimal y)
+        {
+            string result = Convert.ToString(x + y);
+            return result;
+
+        }
+    }
+
+    public class Subtraction : CalculatorBase
+    {
+        public Subtraction()
+        {
+
+        }
+        public override string Operation(decimal x, decimal y)
+        {
+            string result = Convert.ToString(x - y);
+            return result;
+        }
+    }
+
+    public class Multiplication : CalculatorBase
+    {
+        public Multiplication()
+        {
+
+        }
+        public override string Operation(decimal x, decimal y)
+        {
+            string result = Convert.ToString(x * y);
+            return result;
+        }
+    }
+
+    public class Division : CalculatorBase
+    {
+        public Division()
+        {
+
+        }
+        public override string Operation(decimal x, decimal y)
+        {
+            string result = Convert.ToString(x / y);
+            return result;
+        }
+    }
+
+    public class Power : CalculatorBase
+    {
+        public Power()
+        {
+
+        }
+
+        public override string Operation(decimal x, decimal y)
+        {
+            string result = Convert.ToString(Math.Pow( (double)x , (double)y) );
+            return result;
+        }
+    }
+
+    public class SquareRoot : CalculatorBase
+    {
+        public SquareRoot()
+        {
+
+        }
+
+        public virtual string Operation(decimal x)
+        {
+            string result = Convert.ToString(Math.Sqrt( (double)x ));
+            return result;
+        }
+
+        public override string Operation(decimal x, decimal y)
+        {
+            string result = Convert.ToString(Math.Sqrt((double)x));
+            return result;
+        }
+    }
+}
---
+++ Session-06/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session_06
+{
+    public partial class Form1 : Form
+    {
+        Decimal value = 0;
+        string operation = "";
+        bool operationPressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operationPressed))
+            {
+                result.Clear();
+            }      
+                
+            Button b = (Button)sender;
+            result.Text += b.Text;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            //CE clear everything
+            result.Text = "0";
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = Decimal.Parse(result.Text);
+            operationPressed = true;
+        }
+
+        public void button16_Click(object sender, EventArgs e)
+        {
+            Calculator.CalculatorBase calculate;
+
+            switch (operation)
+            {
+                case "+":                    
+                    calculate = new Calculator.Addition();
+                    result.Text = calculate.Operation(value, Decimal.Parse(result.Text));
+                    break;
+                case "-":
+                    calculate = new Calculator.Subtraction();
+                    result.Text = calculate.Operation(value, Decimal.Parse(result.Text));
+                    break;
+                case "*":
+                    calculate = new Calculator.Multiplication();
+                    result.Text = calculate.Operation(value, Decimal.Parse(result.Text));
+                    break;
+                case "/":
+                    calculate = new Calculator.Division();
+                    result.Text = calculate.Operation(value, Decimal.Parse(result.Text));
+                    break;
+                case "sqrt":
+                    calculate = new Calculator.SquareRoot();
+                    result.Text = calculate.Operation(value, Decimal.Parse(result.Text));
+                    break;
+                case "^":
+                    calculate = new Calculator.Power();
+                    result.Text = calculate.Operation(value, Decimal.Parse(result.Text));
+                    break;
+                
+                default:
+                    break;
+            }         
+            operationPressed = false;
+        }
+    }
+}
